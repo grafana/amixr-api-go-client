df4bb3eaadbeabb5ced8968e1655f10d8bd59f07
Julia <ferril.darkdiver@gmail.com>
2022-05-31 14:52:54 +0300

    Improve custom actions with CRUD methods, add tests

--- custom_action_test.go
+++ custom_action_test.go
@@ -51,3 +51,69 @@ func TestListCustomActions(t *testing.T) {
 		t.Errorf("returned\n %+v, \nwant\n %+v", customActions, want)
 	}
 }
+
+func TestCreateCustomAction(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v1/actions/", func(w http.ResponseWriter, r *http.Request) {
+		testRequestMethod(t, r, "POST")
+		fmt.Fprint(w, testCustomActionBody)
+	})
+
+	createOptions := &CreateCustomActionOptions{
+		Name: "Test CustomAction",
+		Webhook: "https://example.com",
+	}
+	customAction, _, err := client.CustomActions.CreateCustomAction(createOptions)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := testCustomAction
+
+	if !reflect.DeepEqual(want, customAction) {
+		t.Errorf("returned\n %+v\n want\n %+v\n", customAction, want)
+	}
+}
+
+func TestDeleteCustomAction(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v1/actions/KGEFG74LU1D8L/", func(w http.ResponseWriter, r *http.Request) {
+		testRequestMethod(t, r, "DELETE")
+	})
+
+	options := &DeleteCustomActionOptions{}
+
+	_, err := client.CustomActions.DeleteCustomAction("KGEFG74LU1D8L", options)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestGetCustomAction(t *testing.T) {
+	mux, server, client := setup(t)
+	defer teardown(server)
+
+	mux.HandleFunc("/api/v1/actions/KGEFG74LU1D8L/", func(w http.ResponseWriter, r *http.Request) {
+		testRequestMethod(t, r, "GET")
+		fmt.Fprint(w, testCustomActionBody)
+	})
+
+	options := &GetCustomActionOptions{}
+
+	customAction, _, err := client.CustomActions.GetCustomAction("KGEFG74LU1D8L", options)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := testCustomAction
+
+	if !reflect.DeepEqual(want, customAction) {
+		t.Errorf("returned\n %+v\n want\n %+v\n", customAction, want)
+	}
+}
