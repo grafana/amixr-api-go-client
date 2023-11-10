6ec4a990c952e561b6b449a59fd19b4cc0c469bf
Vadim Stepanov <vadimkerr@gmail.com>
2023-11-10 13:57:04 +0000

    IntegrationFilter *[]string

--- webhook.go
+++ webhook.go
@@ -27,21 +27,21 @@ type PaginatedWebhooksResponse struct {
 }
 
 type Webhook struct {
-	ID                  string  `json:"id"`
-	Name                string  `json:"name"`
-	Team                string  `json:"team"`
-	Url                 string  `json:"url"`
-	TriggerType         string  `json:"trigger_type"`
-	HttpMethod          string  `json:"http_method"`
-	Data                *string `json:"data"`
-	Username            *string `json:"username"`
-	Password            *string `json:"password"`
-	AuthorizationHeader *string `json:"authorization_header"`
-	TriggerTemplate     *string `json:"trigger_template"`
-	Headers             *string `json:"headers"`
-	ForwardAll          bool    `json:"forward_all"`
-	IntegrationFilter   *string `json:"integration_filter"`
-	IsWebhookEnabled    bool    `json:"is_webhook_enabled"`
+	ID                  string    `json:"id"`
+	Name                string    `json:"name"`
+	Team                string    `json:"team"`
+	Url                 string    `json:"url"`
+	TriggerType         string    `json:"trigger_type"`
+	HttpMethod          string    `json:"http_method"`
+	Data                *string   `json:"data"`
+	Username            *string   `json:"username"`
+	Password            *string   `json:"password"`
+	AuthorizationHeader *string   `json:"authorization_header"`
+	TriggerTemplate     *string   `json:"trigger_template"`
+	Headers             *string   `json:"headers"`
+	ForwardAll          bool      `json:"forward_all"`
+	IntegrationFilter   *[]string `json:"integration_filter"`
+	IsWebhookEnabled    bool      `json:"is_webhook_enabled"`
 }
 
 type ListWebhookOptions struct {
@@ -93,20 +93,20 @@ func (service *WebhookService) GetWebhook(id string, opt *GetWebhookOptions) (*W
 }
 
 type CreateWebhookOptions struct {
-	Name                string  `json:"name"`
-	Team                string  `json:"team"`
-	Url                 string  `json:"url"`
-	TriggerType         string  `json:"trigger_type"`
-	HttpMethod          string  `json:"http_method"`
-	Data                *string `json:"data"`
-	Username            *string `json:"username"`
-	Password            *string `json:"password"`
-	AuthorizationHeader *string `json:"authorization_header"`
-	TriggerTemplate     *string `json:"trigger_template"`
-	Headers             *string `json:"headers"`
-	ForwardAll          bool    `json:"forward_all"`
-	IntegrationFilter   *string `json:"integration_filter"`
-	IsWebhookEnabled    bool    `json:"is_webhook_enabled"`
+	Name                string    `json:"name"`
+	Team                string    `json:"team"`
+	Url                 string    `json:"url"`
+	TriggerType         string    `json:"trigger_type"`
+	HttpMethod          string    `json:"http_method"`
+	Data                *string   `json:"data"`
+	Username            *string   `json:"username"`
+	Password            *string   `json:"password"`
+	AuthorizationHeader *string   `json:"authorization_header"`
+	TriggerTemplate     *string   `json:"trigger_template"`
+	Headers             *string   `json:"headers"`
+	ForwardAll          bool      `json:"forward_all"`
+	IntegrationFilter   *[]string `json:"integration_filter"`
+	IsWebhookEnabled    bool      `json:"is_webhook_enabled"`
 }
 
 // CreateWebhook creates webhook
@@ -132,20 +132,20 @@ func (service *WebhookService) CreateWebhook(opt *CreateWebhookOptions) (*Webhoo
 }
 
 type UpdateWebhookOptions struct {
-	Name                string  `json:"name"`
-	Team                string  `json:"team"`
-	Url                 string  `json:"url"`
-	TriggerType         string  `json:"trigger_type"`
-	HttpMethod          string  `json:"http_method"`
-	Data                *string `json:"data"`
-	Username            *string `json:"username"`
-	Password            *string `json:"password"`
-	AuthorizationHeader *string `json:"authorization_header"`
-	TriggerTemplate     *string `json:"trigger_template"`
-	Headers             *string `json:"headers"`
-	ForwardAll          bool    `json:"forward_all"`
-	IntegrationFilter   *string `json:"integration_filter"`
-	IsWebhookEnabled    bool    `json:"is_webhook_enabled"`
+	Name                string    `json:"name"`
+	Team                string    `json:"team"`
+	Url                 string    `json:"url"`
+	TriggerType         string    `json:"trigger_type"`
+	HttpMethod          string    `json:"http_method"`
+	Data                *string   `json:"data"`
+	Username            *string   `json:"username"`
+	Password            *string   `json:"password"`
+	AuthorizationHeader *string   `json:"authorization_header"`
+	TriggerTemplate     *string   `json:"trigger_template"`
+	Headers             *string   `json:"headers"`
+	ForwardAll          bool      `json:"forward_all"`
+	IntegrationFilter   *[]string `json:"integration_filter"`
+	IsWebhookEnabled    bool      `json:"is_webhook_enabled"`
 }
 
 // UpdateWebhook updates webhook
